97f7c5b75472ed9208626c8143e76a08966da237
GOSID <overheet555@gmail.com>
2025-02-15 22:53:17 +0700

    test

--- lib/page1.dart
+++ lib/page1.dart
@@ -8,3 +8,27 @@ class Page1 extends StatelessWidget {
     return const Placeholder();
   }
 }
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 500,
+            width: 500,
+            color: Colors.amber,
+          )
+        ],
+      ),
+    );
+  }
+}
